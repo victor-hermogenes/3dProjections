3643c6e5e413b59a08363e8e1a6d4c9d0cc8f55e
victor-hermogenes <victorltrgh@gmail.com>
2022-09-14 17:56:57 -0300

    Drawing 3d cube

--- camera.py
+++ camera.py
@@ -13,6 +13,7 @@ class Camera:
         self.v_fov = self.h_fov * (render.HEIGHT / render.WIDTH)
         self.near_plane = 0.1
         self.far_plane = 100
+        self.moving_speed = 0.02
 
     def translate_matrix(self):
         x, y, z, w = self.position
--- tank_projection.py
+++ tank_projection.py
@@ -1,4 +1,6 @@
 from object_3d import *
+from camera import *
+from projections import *
 import pygame as pg
 
 
@@ -13,10 +15,15 @@ class SoftwareRender:
         self.create_objects()
 
     def create_objects(self):
+        self.camera = Camera(self, [0.5, 1, -4])
+        self.projection = Projection(self)
         self.object = Object3D(self)
+        self.object.translate([0.2, 0.4, 0.2])
+        self.object.rotate_y(pi / 6)
 
     def draw(self):
         self.screen.fill(pg.Color('darkslategray'))
+        self.object.draw()
 
     def run(self):
         while True:
